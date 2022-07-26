ab9f5afaa9d8ecff58f30cf844f84a4cd2d40794
Michael Chappelow <uskm1c03@usaws1172.us.kellogg.com>
2022-07-26 16:28:35 +0000

    updated zoltar for syntax tip

--- R/zoltar.R
+++ R/zoltar.R
@@ -7,7 +7,9 @@
 #' 2. Create a local RStudio Connect API key <https://docs.rstudio.com/connect/user/api-keys>
 #' 3. Create an .Renviron file in your Home folder assigning your API key value to the name CONNECT_API_KEY <https://rstats.wtf/r-startup.html>
 #'
-#' @param x A string containing the name of secret to be retrieved (must be wrapped in quotation marks)
+#' @param token character string containing the exact name of secret to be retrieved (if passing value directly must be wrapped in quotation marks to be interpreted as character string)
+#'
+#' @usage zoltar(token)
 #'
 #' @return If name is found, a string containing the secret value else a string stating that zoltar is unable to grant your wish
 #' @export
@@ -16,9 +18,9 @@
 #' zoltar("s3BucketName")
 #' zoltar("MS_SQL_ANALYTICS_DEV_server")
 
-zoltar <- function(x){
+zoltar <- function(token){
   zoltar_speaks <- httr::content(httr::GET(url = "https://rstudioconnect.analytics.kellogg.com/zoltar/wish",
-                          query = list(wish=x),
+                          query = list(wish=token),
                           httr::add_headers(Authorization =
                                               paste0("Key ", Sys.getenv("CONNECT_API_KEY")))),
                 as = "parsed")[[1]]

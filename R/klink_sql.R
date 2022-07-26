4da1f06255ff42d2840f65d15a74edd7b4b2e876
Michael Chappelow <uskm1c03@usaws1172.us.kellogg.com>
2022-07-26 16:00:05 +0000

    updated klink_sql to accept server argument

--- R/klink_sql.R
+++ R/klink_sql.R
@@ -7,24 +7,42 @@
 #' 2. Create a local RStudio Connect API key <https://docs.rstudio.com/connect/user/api-keys>
 #' 3. Create an .Renviron file in your Home folder assigning your API key value to the name CONNECT_API_KEY <https://rstats.wtf/r-startup.html>
 #'
-#' @param environment Valid arguments are "PROD" or "DEV"
-#' @param database Any database in the specified SQL environment should be valid. In current version values of  KG_R_APPS will attempt to use KG_R_APPS svc account credentials, KG_SAS will use KG_SAS credentials, and all other databases will attempt to use KG_ANALYTICS_APPS. If you have suggestions for refining or correcting these functions please contact package authors.
+#' @param environment character string containing desired environment. Valid arguments are "PROD" or "DEV".
+#' @param database character string containing name of desired database. Any database in the specified SQL environment should be valid.
+#' @param server character string containing exact server name in R format (beware of /s). If no argument provided will attempt to use Keystone servers. If server argument is provided the function will attempt to pass specified value into the connection call.
 #'
-#' @return Returns SQL odbc connection object
+#' @usage klink_sql(environment, database, server = NULL)
+#'
+#' @return Returns SQL DBI connection object
 #' @export
 #'
 #' @examples
 #' conn <- klink_sql("DEV", "KG_ANALYTICS_APPS")
 
-klink_sql <- function(environment, database){
+klink_sql <- function(environment, database, server = NULL){
 
   # retrieve wishes using zoltar
   #----------------------------------------------------------------------------
+  # handling ANALYTICSDEV3 (only 1 currently known set)
+  if(!is.null(server)){
+    server_val <- server
+
+    if (server == "USAWSCWSQL5066\\ANALYTICSDEV3" & environment == "DEV"){
+      uid <- klink::zoltar("USAWSCWSQL5066_DEV_userid")
+      pwd <- klink::zoltar("USAWSCWSQL5066_DEV_pwd")
+
+    } else if (server == "USAWSCWSQL5066\\ANALYTICSDEV3" & environment == "PROD"){
+      uid <- klink::zoltar("USAWSCWSQL5066_PROD_userid")
+      pwd <- klink::zoltar("USAWSCWSQL5066_PROD_pwd")}
+  }
+
   # DEV
-  if(environment == "DEV"){
-    # retrieve server
-    server <- klink::zoltar("MS_SQL_ANALYTICS_DEV_server")
-    # retrieve credentials
+  else if(environment == "DEV"){
+
+    # server
+    server_val <- klink::zoltar("MS_SQL_ANALYTICS_DEV_server")
+
+    # retrieve credentials for keystone DBs
     if (database %in% c("KG_R_APPS", "KNA_FIN")) {
       uid <- klink::zoltar("KG_R_APPS_DEV_userid")
       pwd <- klink::zoltar("KG_R_APPS_DEV_pwd")
@@ -34,16 +52,18 @@ klink_sql <- function(environment, database){
       uid <- klink::zoltar("KG_SAS_DEV_userid")
       pwd <- klink::zoltar("KG_SAS_DEV_pwd")
     } else {
-      # !!! using KG_SAS as catch-all/default for now
+      # using KG_SAS as catch-all/default for now
       uid <- klink::zoltar("KG_SAS_DEV_userid")
       pwd <- klink::zoltar("KG_SAS_DEV_pwd")
     }
 
     # PROD
     } else if(environment == "PROD") {
-      # retrieve server
-      server <- klink::zoltar("MS_SQL_ANALYTICS_PROD_server")
-      # retrieve credentials
+
+      # server
+      server_val <- klink::zoltar("MS_SQL_ANALYTICS_PROD_server")
+
+      # retrieve credentials for keystone DBs
       if (database %in% c("KG_R_APPS", "KNA_FIN")) {
         uid <- klink::zoltar("KG_R_APPS_PROD_userid")
         pwd <- klink::zoltar("KG_R_APPS_PROD_pwd")
@@ -53,7 +73,7 @@ klink_sql <- function(environment, database){
         uid <- klink::zoltar("KG_SAS_PROD_userid")
         pwd <- klink::zoltar("KG_SAS_PROD_pwd")
       } else {
-        # !!! using KG_SAS as catch-all/default for now
+        # using KG_SAS as catch-all/default for now
         uid <- klink::zoltar("KG_SAS_PROD_userid")
         pwd <- klink::zoltar("KG_SAS_PROD_pwd")
       }
@@ -61,7 +81,7 @@ klink_sql <- function(environment, database){
       "Error: Invalid environment name. Should be 'DEV' or 'PROD'."
     }
 
-  # connection or zoltar error message handling
+  # connection or zoltar error message return
   #----------------------------------------------------------------------------
   if(grepl("^Error", uid, ignore.case = TRUE)){
     return(uid) # would be error message from zoltar
@@ -72,7 +92,7 @@ klink_sql <- function(environment, database){
         DBI::dbConnect(
           odbc::odbc()
           ,Driver = "freetds" #"SQLServer"
-          ,Server = server
+          ,Server = server_val
           ,Database = database
           ,UID = uid
           ,PWD = pwd

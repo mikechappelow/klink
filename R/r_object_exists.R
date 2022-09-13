a8b0422bf51463992891f1e576ba6b111bf96ea9
Michael Chappelow <uskm1c03@usaws1171.us.kellogg.com>
2022-09-13 17:59:53 +0000

    renamed object_exists fun

--- R/r_object_exists.R
+++ R/r_object_exists.R
@@ -1,17 +1,17 @@
-#' object_exists
+#' r_object_exists
 #' @description checks for existence of object when name passed without quotes
 #'
 #' @param object_name name of object (without quotation marks)
 #'
-#' @usage object_exists(object_name)
+#' @usage r_object_exists(object_name)
 #'
 #' @return If object exists will return TRUE else FALSE
 #' @export
 #'
 #' @examples
-#' object_exists(test)
-#' object_exists(test2)
+#' r_object_exists(test)
+#' r_object_exists(test2)
 
-object_exists <- function(object_name){
+r_object_exists <- function(object_name){
   exists(as.character(substitute(object_name)))
 }
